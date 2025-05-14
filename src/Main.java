55748afadaca3c0327e74761962592e2fb43a760
Dmitry1987-hub <parkhomin1987@mail.ru>
2025-05-14 13:21:58 +0300

    Homework 2

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,87 @@ public class Main {
     public static void main(String[] args) {
 
 
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+
+
+
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+
+        var boxer1 = 78.2;
+        System.out.println(boxer1);
+        var boxer2 = 82.7;
+        System.out.println(boxer2);
+        var result = boxer1 + boxer2;
+        System.out.println("boxer1 + boxer2 = " + result);
+
+
+        var result2 = boxer2 - boxer1;
+        System.out.println("boxer2 - boxer1 = " + result2);
+
+
+        var totalWeight = (boxer1 + boxer2) % result;
+        System.out.println("разница" + totalWeight + "кг!");
+
+
+        var tiemHours = 640;
+        System.out.println(tiemHours);
+        var employee = 8;
+        System.out.println(employee);
+        var result4 = tiemHours / employee;
+        System.out.println(" работников в компании " + result4 +  " человек ");
+        var human = 80;
+        System.out.println(human);
+        human = human + 94;
+        System.out.println(human);
+        var result5 = employee * human;
+        System.out.println("Если в компании работает сотрудников " + human + " то всего часов может быть распределено между сотрудниками " + result5 + " часа!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
